862218b38cc2666f221f4468eda2efdb2a0c9a5d
Vincenzo Abete <abetevincenzo@gmail.com>
2020-02-04 12:10:15 +0000

    Tested object initialisation. Pass :)

--- marinella.cpp
+++ marinella.cpp
@@ -2,10 +2,31 @@
 #include <cstdlib>
 #include <cstring>
 
+#include "Boat.h"
+#include "MotorBoat.h"
+#include "NarrowBoat.h"
+#include "SailingBoat.h"
+
 using namespace std;
 
 int main(void)
 {
+	MotorBoat* motorBoat = new MotorBoat(5,1.5);
+	NarrowBoat* narrowBoat = new NarrowBoat(6, 1.6);
+	SailingBoat* sailingBoat = new SailingBoat(7, 1.7);
+
+	cout << "\n\nThe first boat is a motorboat" << endl;
+	cout << "Lenght: " << motorBoat->getLenght() << endl;
+	cout << "Depth: " << motorBoat->getDepth() << endl;
+
+	cout << "\n\nThe second boat is a narrowboat" << endl;
+	cout << "Lenght: " << narrowBoat->getLenght() << endl;
+	cout << "Depth: " << narrowBoat->getDepth() << endl;
+
+	cout << "\n\nThe third boat is a sailingboat" << endl;
+	cout << "Lenght: " << sailingBoat->getLenght() << endl;
+	cout << "Depth: " << sailingBoat->getDepth() << endl;
+
 	system("PAUSE");
 	return 0;
 }
