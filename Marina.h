d0afcc1242739e3510550e0957ade71f40bf93fa
Vincenzo Abete <abetevincenzo@gmail.com>
2020-02-11 10:56:22 +0000

    Simple switch statement for boat creation

---
+++ Marina.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "Boat.h"
+
+class Marina
+{
+
+	// CONSIDER MAKING THIS IS SINGLETON
+
+private:
+	float lenght;
+
+public:
+	Marina();
+	~Marina();
+
+	float getRemainingSpace()
+	{
+	}
+};
+
--- marinella.cpp
+++ marinella.cpp
@@ -7,25 +7,70 @@
 #include "NarrowBoat.h"
 #include "SailingBoat.h"
 
+#include "Marina.h"
+
 using namespace std;
 
 int main(void)
 {
-	MotorBoat* motorBoat = new MotorBoat(5,1.5);
-	NarrowBoat* narrowBoat = new NarrowBoat(6, 1.6);
-	SailingBoat* sailingBoat = new SailingBoat(7, 1.7);
+	int choice;
 
-	cout << "\n\nThe first boat is a motorboat" << endl;
-	cout << "Lenght: " << motorBoat->getLenght() << endl;
-	cout << "Depth: " << motorBoat->getDepth() << endl;
+	// =================== MENU ======================
 
-	cout << "\n\nThe second boat is a narrowboat" << endl;
-	cout << "Lenght: " << narrowBoat->getLenght() << endl;
-	cout << "Depth: " << narrowBoat->getDepth() << endl;
+	cout << "@@@@@@@@@@@@@@ MARINELLA (R) - Marina Berth Booking System @@@@@@@@@@@@@" << endl;
+	cout << "\nPlease select an option from the menu below..." << endl;
+	cout << "------------------------------------------------------------------------" << endl;
+	cout << "|  1. New booking  |  2. Delete record  |  3. Show marina  |  4. Exit  |" << endl;
+	cout << "------------------------------------------------------------------------" << endl;
 
-	cout << "\n\nThe third boat is a sailingboat" << endl;
-	cout << "Lenght: " << sailingBoat->getLenght() << endl;
-	cout << "Depth: " << sailingBoat->getDepth() << endl;
+	cin >> choice;
+
+	// ===============================================
+
+	if (choice == 1)
+	{
+		int boatChoice;
+
+		cout << "Please select type of boat:" << endl;
+		cout << "1. Motorboat" << endl;
+		cout << "2. Narrowboat" << endl;
+		cout << "3. Sailingboat" << endl;
+		cin >> boatChoice;
+
+		float boatLenght, boatDepth;
+		cout << "Please enter the lenght of the boat: ";
+		cin >> boatLenght;
+		cout << "Please enter the depth of the boat: ";
+		cin >> boatDepth;
+
+		switch (boatChoice)
+		{
+			case 1: 
+			{
+				MotorBoat* motorBoat = new MotorBoat(boatLenght, boatDepth);
+				cout << "\n\nDetails of the motorboat" << endl;
+				cout << "Lenght: " << motorBoat->getLenght() << endl;
+				cout << "Depth: " << motorBoat->getDepth() << endl;
+				break;
+			}
+			case 2: 
+			{
+				NarrowBoat* narrowBoat = new NarrowBoat(boatLenght, boatDepth);
+				cout << "\n\nDetails of the narrowboat" << endl;
+				cout << "Lenght: " << narrowBoat->getLenght() << endl;
+				cout << "Depth: " << narrowBoat->getDepth() << endl;
+				break;
+			}
+			case 3:
+			{
+				SailingBoat* sailingBoat = new SailingBoat(boatLenght, boatDepth);
+				cout << "\n\nDetails of the sailingboat" << endl;
+				cout << "Lenght: " << sailingBoat->getLenght() << endl;
+				cout << "Depth: " << sailingBoat->getDepth() << endl;
+				break;
+			}
+		}
+	}
 
 	system("PAUSE");
 	return 0;
